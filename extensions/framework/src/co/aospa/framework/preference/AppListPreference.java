0c0d57b1658e2704e3c1bb1c3b4ba2cd199de651
xenxynon <flynryder427@gmail.com>
2024-11-26 06:30:23 +0000

    extensions: rename packages  it worked as is but eh anyways

--- extensions/framework/src/co/aospa/framework/preference/AppListPreference.java
+++ extensions/framework/src/co/aospa/framework/preference/AppListPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.derpfest.support.preferences;
+package co.aospa.framework.preference;
 
 import android.content.Context;
 import androidx.preference.Preference;
